c56616bbc5bb9d4cae4afdcae5fc61c6eb6557db
Calebe Andrade <calebe.correia@aluno.senai.br>
2025-05-21 09:42:55 -0500

    Segundo commmit para salvar alterações em atividade

---
+++ script.js
@@ -0,0 +1,11 @@
+function sim() {
+    alert("Você aceitou namorar comigo! :)");
+
+    location.href = "https://www.youtube.com/watch?v=izGwDsrQ1eQ";
+};
+
+function nao() {
+    alert("Você não aceitou namorar comigo :(")
+
+    location.href = "https://www.youtube.com/watch?v=G0Hkln_DFO0";
+}
